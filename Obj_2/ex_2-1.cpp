9da824786bf861a53819902543eba7eb4c1db646
LitchiLy <liyang20182@email.szu.edu.cn>
2023-09-05 21:55:34 +0800

    On branch office Your branch is up to date with 'origin/office'.
    Changes to be committed:
    	modified:   C++_Note.md
    	new file:   Obj_2/ex_2-1.cpp
    	new file:   Obj_2/ex_2-1.exe

--- Obj_2/ex_2-1.cpp
+++ Obj_2/ex_2-1.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+<<<<<<< HEAD
    //  // 1
    //  unsigned u = 10, u2 = 42;
    //  std::cout << u2 - u << std::endl;
@@ -41,5 +42,26 @@ int main()
 	std::cout << "2\t\x4d\n";
 
 	_getch();
+=======
+    // 1
+    unsigned u = 10, u2 = 42;
+    std::cout << u2 - u << std::endl;
+    std::cout << u - u2 << std::endl;
+    /*
+        无符号的定义符号如果超出了展示范围, 那么就会取模
+    */
+
+   int i = 10, i2 = 42;
+   std::cout << i2 - i << std::endl;
+   std::cout << i - i2 << std::endl;
+   std::cout << i - u << std::endl;
+   std::cout << u - i << std::endl;
+   std::cout << u - i2 << std::endl;
+   /*
+       无符号与有符号的加减乘除中, 无符号的优先级更大, 会将有符号的数转化成无符号(负数, 减法将取模), 然后再相加.
+   */
+    
+   _getch();
+>>>>>>> 9d38c81 (On branch office)
    return 0;
 }
